a21414706e6df12033fea2bb96a22615f74839f0
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-10-29 13:09:25 +0530

    project complete

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,33 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "card2"
+    @State var cpuCard = "card3"
+    @State var playerScore = 0
+    @State var cpuScore = 0
     var body: some View {
         ZStack{
             Image("background-plain").resizable().ignoresSafeArea()
             VStack{
                 Image("logo")
                 HStack{
-                    Image("card2")
                     Spacer()
-                    Image("card3")
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
                 }.padding()
-                Image("button")
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
                 HStack{
                     Spacer()
                     VStack{
                         Text("Player")
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.title)
                     }
                     Spacer()
                     VStack{
                         Text("CPU")
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.title)
                     }
                     Spacer()
                 }
             }.foregroundStyle(.white)
+        }
+    }
+    func deal()
+    {
+        var random1 = Int.random(in: 2...14)
+        var random2 = Int.random(in: 2...14)
+        playerCard = "card" + String(random1)
+        cpuCard = "card" + String(random2)
+        if random1 > random2
+        {
+            playerScore += 1
+        }
+        else if random1 < random2
+        {
+            cpuScore += 1
         }
     }
 }
